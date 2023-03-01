8e265f4d2cb861f1a9065f2c3364a64068e89cb2
Sang Min Park <125769061+s010419@users.noreply.github.com>
2023-03-01 10:28:46 +0900

    [Bronze V] Title: 영수증, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,11 @@
+X = int(input())
+N = int(input())
+
+price = 0
+for i in range(N):
+    a, b = map(int, input().split())
+    price += a*b
+if X == price:
+    print("Yes")
+else:
+    print("No")
